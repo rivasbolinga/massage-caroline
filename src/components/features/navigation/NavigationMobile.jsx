818bf3c966114c596783c178df7287351c84430b
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-03-29 19:48:29 +0530

    Mobile nav item reodering

--- src/components/features/navigation/NavigationMobile.jsx
+++ src/components/features/navigation/NavigationMobile.jsx
@@ -13,19 +13,14 @@ const NavigationMobile = () => {
 			<div className="burger">
 				<Hamburger toggled={isOpen} toggle={setOpen} direction="right" />
 			</div>
-			{isOpen && <ul className="menu" onClick={()=>setOpen(false)}>
+			{isOpen && <ul className="menu" onClick={() => setOpen(false)}>
 				<li className="nav-item">
 					<Link to="/">Placeholder</Link>
 				</li>
 				<li className="nav-item">
 					<Link to="/">Home</Link>
 				</li>
-				<li className="nav-item">
-					<Link to="/about">About us</Link>
-				</li>
-				<li className="nav-item">
-					<Link to="/contact">Contact us</Link>
-				</li>
+
 				<li className="nav-item">
 					<Link to="/bookings">Bookings</Link>
 				</li>
@@ -37,6 +32,12 @@ const NavigationMobile = () => {
 				</li>
 				<li className="nav-item">
 					<Link to="/gift-card">Gift card</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/about">About us</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/contact">Contact us</Link>
 				</li>
 			</ul>}
 		</NavWrapper>
