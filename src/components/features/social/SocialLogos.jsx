e7c2d5dfcf46dcbf58bc11cf29d4090653431119
Andrea <andrearivas4cinfor@gmail.com>
2023-04-01 20:10:16 +0200

    Add social logos component

--- src/components/features/social/SocialLogos.jsx
+++ src/components/features/social/SocialLogos.jsx
@@ -1,7 +1,28 @@
-import BsFacebook from 'react-icons';
+import { BsFacebook, BsWhatsapp, BsMailbox, BsInstagram } from 'react-icons/bs'
+import styled from 'styled-components';
+import { AiOutlineMail } from 'react-icons/ai';
 
 const SocialLogos = () => {
-  return <BsFacebook />
+  return (
+    <Wrapper className="social-logos-container">
+      <BsFacebook className="logo" />
+      <BsWhatsapp className="logo" />
+      <BsInstagram className="logo" />
+      <AiOutlineMail className="logo mail" />
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  .logo {
+    font-size: 30px
+  }
+  .mail {
+    font-size: 35px;
+  }
+`
 
 export default SocialLogos
