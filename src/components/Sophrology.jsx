721d7e83cc0d0dc09b97ed4944e22c722b8325d6
Andrea <andrearivas4cinfor@gmail.com>
2023-04-05 11:06:39 +0200

    Add content to sp page

--- src/components/Sophrology.jsx
+++ src/components/Sophrology.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
-
+import { useTranslation } from 'react-i18next'
 const Sophrology = () => {
+	const { t } = useTranslation()
 	return (
-		<div>
-			<h1>Hello form Sophrology</h1>
-		</div>
-	)
+    <div>
+      <header>
+        <h1 className="sophrology-title">{t('sophrology.sophrology')}</h1>
+        <div className="horizontal-line"></div>
+      </header>
+      <main className="sophrology-main">
+        <div className="what-is">
+          <h2 className="what-is-question">{t('sophrology.what-is')}</h2>
+          <p className="what-is-text">{t('sophrology.what-description')}</p>
+        </div>
+        <div className="who-for">
+          <h2 className="who-for-question">{t('sophrology.who-for')}</h2>
+          <p className="who-description">
+            {t('sophrology.who-for-description')}
+          </p>
+          <div className="offer-container">
+            <p className="offer-text">{t('sophrology.offer')}</p>
+            <p className="duration-text">{t('sophrology.duration')}</p>
+          </div>
+          <div className="sessions">
+            <p className="first-session">
+              <p className="offer-text">{t('sophrology.first-session')}</p>
+            </p>
+            <p className="follow-up">
+              <p className="offer-text">{t('sophrology.follow-up')}</p>
+            </p>
+          </div>
+          <button className="book-now-btn"></button>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default Sophrology
--- src/components/aboutPage/About.jsx
+++ src/components/aboutPage/About.jsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 import AboutImages from '../features/aboutImages/AboutImages'
 
 const About = () => {
-  const { t } = useTranslation('translation')
+  const { t } = useTranslation()
 	return (
     <>
       <main>
