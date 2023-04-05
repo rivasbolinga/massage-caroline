e05796911fd4c8c24ae40d82292b3dcaaa54f6c6
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-05 11:06:24 +0530

    carousel Title

--- src/components/features/home-carousel/Carousel.jsx
+++ src/components/features/home-carousel/Carousel.jsx
@@ -6,10 +6,22 @@ import banner3 from '../../../assets/massage-banners/banner-3.avif'
 import styled from 'styled-components';
 
 const Carousel = () => {
-  const images = [
-    banner1,
-    banner2,
-    banner3
+  const peelers = [
+    {
+      image: banner1,
+      quote: `Want to Relax, <br /> you are at right place`,
+      style: {backgroundColor:'rgba(255, 255, 255, 20%)',color: '#D25C33', top: 0, right: 0, borderRadius: "2rem 0 0 2rem"}
+    },
+    {
+      image: banner2,
+      quote: `We don't compromise <br />in costumer comfort`,
+      style: {backgroundColor:'rgba(255, 255, 255, 20%)',color: '#30221E', bottom: 0, right: 0, borderRadius: "2rem 0 0 2rem"}
+    },
+    {
+      image: banner3,
+      quote: `We can beautify your <br /> each muscles`,
+      style: {backgroundColor:'rgba(255, 255, 255, 20%)',color: '#D25C33', bottom: 0, left: 0, borderRadius: "0 2rem 2rem 0"}
+    }
   ];
 
   const settings = {
@@ -19,28 +31,47 @@ const Carousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
   }
   return (
     <CarouselWrap>
       <Slider {...settings} >
-        {images.map((item, index) => <img key={index} src={item} alt="Banner Carousel" />)}
+        {peelers.map((item, index) => (
+        <div className="carousel">
+          <img key={index} src={item.image} alt="Banner Carousel" />
+          <h1 style={item.style} className="carousel-title" dangerouslySetInnerHTML={{ __html: item.quote }}></h1>
+        </div>))}
       </Slider>
     </CarouselWrap>
   )
 }
 
 const CarouselWrap = styled.div`
+  .carousel {
+    position: relative;
+    width: 100%;
+    height: 80vh;
+  }
   .slick-slider {
     width: 100%;
     margin: 0;
     img {
       width: 100%;
       height: 80vh;
+      position: absolute;
       margin: auto 0;
+      z-index: -1;
       @media screen and (max-width: 768px) {
         height: 90vh;
       }
+    }
+
+    h1 {
+      display: inline-block;
+      position: absolute;
+      z-index: 3;
+      font-size: 5rem;
+      padding: 1rem;
     }
   }
 
