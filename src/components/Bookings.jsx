209521dbb18a0a65be9894e9d9c7101fc5adb3ae
Andrea <andrearivas4cinfor@gmail.com>
2023-04-05 19:51:17 +0200

    Add calendar

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { Helmet } from 'react-helmet'
+import { InlineWidget } from 'react-calendly'
 const Bookings = () => {
-	return (
-		<div>
-			<h1>Hello form Bookings</h1>
-		</div>
-	)
+  return (
+    <div>
+      <h1>k</h1>
+      <InlineWidget url="https://calendly.com/andrearivas4cinfor/30min" />
+    </div>
+  )
 }
+
 
 export default Bookings
