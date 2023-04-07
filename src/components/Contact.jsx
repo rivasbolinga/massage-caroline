d99a0aeb8fc4094ae287d1ab517ae4e72a6fd4d9
Andrea <andrearivas4cinfor@gmail.com>
2023-04-07 12:40:14 +0200

    Finish skeleton

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,10 +26,46 @@ const Contact = () => {
         </div>
       </article>
       <article className="location-container">
-        <h2>{t('.contact.address')}</h2>
+        <h2>{t('contact.address')}</h2>
         <p className="location-text">606 route de la Télécabine 01170 Crozet</p>
-        <div className='map-container'>map here</div>
+        <div className="map-container">map here</div>
       </article>
+      <article className="contact-container">
+        <article className="phone-container">
+          <h3 className="phone-title">{t('contact.phone')}</h3>
+          <p className="phone-number">0033/6 74 81 20 09</p>
+          <p className="contact-text">{t('contact.contact-text')}</p>
+        </article>
+      </article>
+       <div className="footer-item form">
+          <h3>Developer's Contact</h3>
+          <form action="https://formspree.io/f/xqkoyerk" method="post">
+            <div className="from-container">
+              <input
+                name="Visitor Name"
+                type="text"
+                maxLength="30"
+                placeholder="Your Name"
+                required
+              />
+              <input
+                name="Purpose"
+                type="text"
+                maxLength="70"
+                placeholder="Purpose of Contact"
+                required
+              />
+              <textarea
+                name="Massage"
+                maxLength="300"
+                placeholder="Massage"
+                type="text"
+                required
+              />
+            </div>
+            <button type="submit">contact Developers</button>
+          </form>
+        </div>
     </section>
   )
 }
