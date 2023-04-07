f8683b66c052ced46b988f4f6607fb3c3ecb1230
Andrea <andrearivas4cinfor@gmail.com>
2023-04-07 12:30:49 +0200

    Add location skeleton

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,9 @@ const Contact = () => {
    const { t } = useTranslation()
   return (
     <section className="contact-section">
-      <article className="contact-title">Contact</article>
+      <article className="contact-title-container">
+        <h1 className="contact-title">Contact</h1>
+      </article>
       <article className="hours-location-container">
         <div className="opening-hours-container">
           <h2 className="opening-hours-title">{t('contact.opening-hours')}</h2>
@@ -19,13 +21,15 @@ const Contact = () => {
               {t('contact.saturday-wednesday-hours')}
             </p>
             <h3 className="sunday">{t('contact.sunday')}</h3>
-            <p className="close">
-              {t('contact.close')}
-            </p>
+            <p className="close">{t('contact.close')}</p>
           </div>
         </div>
       </article>
-      <article className="contact-container"></article>
+      <article className="location-container">
+        <h2>{t('.contact.address')}</h2>
+        <p className="location-text">606 route de la Télécabine 01170 Crozet</p>
+        <div className='map-container'>map here</div>
+      </article>
     </section>
   )
 }
