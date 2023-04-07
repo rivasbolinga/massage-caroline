f619025646058abd0d27296ee39647cf9f0b5d20
Andrea <andrearivas4cinfor@gmail.com>
2023-04-07 12:03:13 +0200

    starting skeletn

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <main>
-      <h1>Hello from contact</h1>
-    </main>
+    <section className="contact-section">
+      <article className="hours-location-container">
+        <div className='opening-hours-container'>
+          <h2 className='opening-hours-title'>
+            
+          </h2>
+        </div>
+      </article>
+      <article className="contact-container">
+
+      </article>
+    </section>
   )
 }
 
