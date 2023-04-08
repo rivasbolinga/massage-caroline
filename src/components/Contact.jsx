905aaad3ddda3934b9f23dcb972be40fc2dcce6e
Andrea <andrearivas4cinfor@gmail.com>
2023-04-08 12:51:10 +0200

    Style opening hours contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import SocialLogos from './features/social/SocialLogos';
 import './styles/Contact.scss';
-
+import { BiTimeFive } from 'react-icons/bi';
 const Contact = () => {
    const { t } = useTranslation()
   return (
@@ -10,33 +10,46 @@ const Contact = () => {
       <article className="contact-title-container">
         <h1 className="contact-title">Contact</h1>
       </article>
-      <article className="hours-location-container">
-        <div className="opening-hours-container">
-          <h2 className="opening-hours-title">{t('contact.opening-hours')}</h2>
-          <div className="opening-hours-text-container">
-            <h3 className="week-days">{t('contact.week-days')}</h3>
-            <h3 className="saturday-wednesday">
-              {t('contact.saturday-wednesday')}
-            </h3>
-            <h3 className="sunday">{t('contact.sunday')}</h3>
- 
+      <main className="contact-main">
+        <article className="hours-location-container">
+          <div className="opening-hours-container">
+            <h2 className="opening-hours-title">
+              {t('contact.opening-hours')}
+            </h2>
+            <div className="title-line"></div>
+            <div className="opening-hours-text-container">
+              <h3 className="week-days">{t('contact.week-days')}</h3>
+              <div className="hours-div">
+                <BiTimeFive className='time-icon' />
+                <p className="hours">7am - 19pm</p>
+              </div>
+              <h3 className="saturday-wednesday">
+                {t('contact.saturday-wednesday')}
+              </h3>
+              <div className="hours-div">
+                <BiTimeFive className='time-icon' />
+                <p className="hours">10am - 13pm</p>
+              </div>
+              <h3 className="sunday">{t('contact.sunday')}</h3>
+            </div>
           </div>
-        </div>
-      </article>
-      <article className="location-container">
-        <h2>{t('contact.address')}</h2>
-        <p className="location-text">606 route de la Télécabine 01170 Crozet</p>
-        <div className="map-container">map here</div>
-      </article>
-      <article className="contact-container">
-        <article className="phone-container">
-          <h3 className="phone-title">{t('contact.phone')}</h3>
-          <p className="phone-number">0033/6 74 81 20 09</p>
-          <p className="contact-text">{t('contact.contact-text')}</p>
-          <SocialLogos />
         </article>
-      </article>
-       <div className="footer-item form">
+        <article className="location-container">
+          <h2>{t('contact.address')}</h2>
+          <p className="location-text">
+            606 route de la Télécabine 01170 Crozet
+          </p>
+          <div className="map-container">map here</div>
+        </article>
+        <article className="contact-container">
+          <article className="phone-container">
+            <h3 className="phone-title">{t('contact.phone')}</h3>
+            <p className="phone-number">0033/6 74 81 20 09</p>
+            <p className="contact-text">{t('contact.contact-text')}</p>
+            <SocialLogos />
+          </article>
+        </article>
+        <div className="footer-item form">
           <h3>Contact</h3>
           <form action="https://formspree.io/f/xqkoyerk" method="post">
             <div className="from-container">
@@ -65,6 +78,7 @@ const Contact = () => {
             <button type="submit">contact Developers</button>
           </form>
         </div>
+      </main>
     </section>
   )
 }
