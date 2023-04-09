f5dfe2a40200b38be7e35dee79446583a05ee914
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-09 18:00:39 +0530

    massage redux data

--- src/components/features/home-sliders/SliderComponent.jsx
+++ src/components/features/home-sliders/SliderComponent.jsx
@@ -1,112 +1,67 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
-import ProductCard from "./ProductCard";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./styles.scss";
-import TitleWrap from "../../styles/HomeTitle";
-import img1 from '../../../assets/home-massage-pics/massage-1.jpg'
-import img2 from '../../../assets/home-massage-pics/massage-2.jpg'
-import img3 from '../../../assets/home-massage-pics/massage-4.jpg'
-import img4 from '../../../assets/home-massage-pics/massage-3.jpg'
-import img5 from '../../../assets/home-massage-pics/massage-5.jpg'
-import img7 from '../../../assets/massage-banners/banner-2.jpg'
-import img8 from '../../../assets/massage-banners/banner-1.webp'
-import img9 from '../../../assets/massage-banners/banner-3.avif'
-import img10 from '../../../assets/images/image5.jpeg'
-import backgroundImage from "../../../assets/massage-banners/background-makups.jpg";
+import React from 'react'
+import { useSelector } from 'react-redux';
+import backgroundImage from "../../../assets/massage-banners/background-makups.jpg"
+import TitleWrap from '../../styles/HomeTitle';
+import Slider from 'react-slick';
+import ProductCard from './ProductCard';
 
-class ProductSlider extends Component {
-  constructor() {
-    super();
-    this.state = {
-      slides: [
-        {
-          img: img1
-        },
-        {
-          img: img2
-        },
-        {
-          img: img3
-        },
-        {
-          img: img4
-        },
-        {
-          img: img5
-        },
-        {
-          img: img7
-        },
-        {
-          img: img8
-        },
-        {
-          img: img9
-        },
-        {
-          img: img10
+const ProductSlider = () => {
+  const massagesData = useSelector(store => store.massageReducer.massagesData)
+  console.log(massagesData)
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    initialSlide: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
         }
-      ]
-    };
-  }
-  render() {
-    var settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 2,
-      initialSlide: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 426,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
         }
-      ]
-    };
-    return (
-      <div id="massages">
-        <img src={backgroundImage} alt="" className="backgroundImage" />
-        <TitleWrap className="about-title">
-          <h2>Our Massages</h2>
-        </TitleWrap>
-        <Slider {...settings}>
-          {this.state.slides.map((slide, index) => {
-            return (
-              <div data-aos="flip-left" data-aos-duration="2000" key={index}>
-                <ProductCard imgSrc={slide.img} />
-                {/* <img src={slide.img} alt={`slide${index}`} /> */}
-              </div>
-            );
-          })}
-        </Slider>
-      </div>
-    );
-  }
+      },
+      {
+        breakpoint: 426,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <div id="massages">
+      <img src={backgroundImage} alt="" className="backgroundImage" />
+      <TitleWrap className="about-title">
+        <h2>Our Massages</h2>
+      </TitleWrap>
+      <Slider {...settings}>
+        {massagesData.map((slide, index) => {
+          return (
+            <div data-aos="flip-left" data-aos-duration="2000" key={index}>
+              <ProductCard imgSrc={slide.image} />
+              {/* <img src={slide.img} alt={`slide${index}`} /> */}
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  )
 }
 
-export default ProductSlider;
+export default ProductSlider
--- src/components/redux/massages/massageSlice.js
+++ src/components/redux/massages/massageSlice.js
@@ -1,108 +1,109 @@
 import { createSlice } from "@reduxjs/toolkit";
+import img1 from "../../../assets/customer.png"
 
 const initialState = {
-  massages: {
-    ayurvedic: [
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-    ],
-    sports: [
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-    ],
-    beauty: [
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-      {
-        id: 1,
-        title: "Massage Title",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
-        prices: [10, 20, 30],
-        time: ["30 min", "1 hour", "2 hour"],
-        image: "../../assets/customer.png",
-        type: "ayurvedic",
-      },
-    ],
-  },
+  massagesData: [
+    {
+      id: 0,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "Sports",
+    },
+    {
+      id: 1,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "Sports",
+    },
+    {
+      id: 2,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "Sports",
+    },
+    {
+      id: 3,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "Sports",
+    },
 
-  detailsTypeId: {type: null, id: null},
+    {
+      id: 4,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "Beautify",
+    },
+    {
+      id: 5,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "Beautify",
+    },
+    {
+      id: 6,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "Beautify",
+    },
+
+    {
+      id: 7,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "ayurvedic",
+    },
+    {
+      id: 8,
+      title: "Massage Title",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!",
+      prices: [10, 20, 30],
+      time: ["30 min", "1 hour", "2 hour"],
+      image: img1,
+      type: "ayurvedic",
+    },
+  ],
+
+
+  detailsTypeId: null,
 }
 
 const massagesSlice = createSlice({
   name: "redux/massagesSlice",
   initialState,
   reducers: {
-    setDeatils: () => {
-      console.log("hello")
+    setDetails: (state, {payload}) => {
+      return {
+        ...state,
+        detailsTypeId: payload,
+      }
     }
   }
 })
 
-export const { setDeatils } = massagesSlice.actions;
+export const { setDetails } = massagesSlice.actions;
 export default massagesSlice.reducer;
