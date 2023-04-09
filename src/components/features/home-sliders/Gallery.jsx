80b8108e114e2b2cbf8a4664f6e33cfbe5551658
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-09 09:47:08 +0530

    Ressponsive HOME gallery

--- src/components/features/home-sliders/Gallery.jsx
+++ src/components/features/home-sliders/Gallery.jsx
@@ -6,6 +6,7 @@ import img2 from '../../../assets/home-massage-pics/massage-2.jpg'
 import img3 from '../../../assets/home-massage-pics/massage-4.jpg'
 import img4 from '../../../assets/home-massage-pics/massage-3.jpg'
 import img5 from '../../../assets/home-massage-pics/massage-5.jpg'
+import img6 from '../../../assets/home-massage-pics/banner.png'
 
 const Gallery = () => {
   return (
@@ -23,12 +24,17 @@ const Gallery = () => {
           </div>
         </div>
         <div className="container-item-big">
-          <div className="image-scale-hover">
-            <img src={img3} alt="" />
+          <div className="image-scale-hover big">
+            <div className="big-image">
+              <img src={img3} alt="" />
+            </div>
             <div className="gallery-pealed">
-              <TitleWrap><h2>we love to hear feedback</h2></TitleWrap >
-              <TitleWrap ><h2>Want to try</h2></TitleWrap>
-              <i className="fa-solid fa-handshake"></i>
+              <img src={img6} alt="" className="banner" />
+              <div className="info">
+                <TitleWrap><h2>we love to hear feedback</h2></TitleWrap >
+                <TitleWrap ><h2>Want to try</h2></TitleWrap>
+                <i className="fa-solid fa-handshake"></i>
+              </div>
             </div>
           </div>
         </div>
@@ -49,29 +55,47 @@ const WrapSection = styled.section`
   width: 100%;
   margin: auto;
   padding: 0;
-  img {
+  img:not(.banner) {
     width: 100%;
     height: 100%;
+    transition: all 1s;
+
+    &:hover {
+      transform: scale(1.5,1.5);
+    }
   }
   .gallery {
     display: flex;
     gap: 1rem;
+    @media screen and (max-width: 768px) {
+      flex-direction: column;
+    }
   }
 
   .image-scale-hover {
     width:100%;
+    overflow: hidden;
   }
-
+  
+  .big {
+    height: 100%;
+  }
   .container-item {
     width: 30%;
+    @media screen and (max-width: 768px) {
+      display: flex;
+      width: 100%;
+    }
   }
   .container-item-big {
     width: 40%;
     h2{
-      font-size: 2rem;
+      font-size: 1.9rem;
       font-weight: 800;
-      margin: .5rem !important;
+      margin: 0.8 .5rem !important;
       padding: .5rem;
+      text-align: left;
+      color: #D25C33;
 
       @media screen and (max-width: 1024px) {
         font-size: 1.2rem;
@@ -81,8 +105,27 @@ const WrapSection = styled.section`
     i {
       font-size: 2rem;
       text-align: center;
+      padding: 1rem;
+    }
+
+    @media screen and (max-width: 768px) {
+      width: 100%;
+    }
+
+    .big-image {
+      overflow: hidden;
+    }
+
+    .gallery-pealed {
+      position: relative;
+      .banner {
+        position: absolute;
+        width: 100%;
+        z-index: -3;
+      }
     }
   }
+  
 `
 
 export default Gallery
