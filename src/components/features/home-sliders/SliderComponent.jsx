d179272dbb6bcad904fe29b41ae23fbabdf71e9e
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-22 14:26:37 +0530

    navigation gap enhanced

--- src/components/features/home-sliders/SliderComponent.jsx
+++ src/components/features/home-sliders/SliderComponent.jsx
@@ -16,13 +16,13 @@ const ProductSlider = () => {
     slidesToScroll: 2,
     initialSlide: 1,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 3000,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
         },
@@ -31,8 +31,9 @@ const ProductSlider = () => {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           initialSlide: 2,
+          infinite: true,
         },
       },
       {
@@ -40,6 +41,7 @@ const ProductSlider = () => {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          infinite: true,
         },
       },
     ],
