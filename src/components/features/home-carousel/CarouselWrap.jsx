a792a3666acf10de4ec67c95d86f0fc8739653f8
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-22 20:36:35 +0530

    responssive carsousel with animations

--- src/components/features/home-carousel/CarouselWrap.jsx
+++ src/components/features/home-carousel/CarouselWrap.jsx
@@ -6,8 +6,17 @@ const CarouselWrap = styled.div`
     50% {background-position: 100% 50%;}
     100% {background-position: 0% 50%;}
   }
+  @keyframes boxShadow {
+    0% {box-shadow: 0 15px 15px rgba(255, 219, 172, .7);}
+    25% {box-shadow: 15px 0 15px rgba(255, 219, 172, .7);}
+    50% {box-shadow: 0 -15px 15px rgba(255, 219, 172, .7);}
+    75% {box-shadow: -15px 0 15px rgba(255, 219, 172, .7);}
+    100% {box-shadow: 0 15px 15px rgba(255, 219, 172, .7);}
+  }
 position: relative;
   .center-hero {
+    width: 90%;
+    max-width: 545px;
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
@@ -22,6 +31,7 @@ position: relative;
     background: linear-gradient(-45deg, rgba(241, 194, 125, 50%), rgba(141, 85, 36, 50%));
     background-size: 400% 400%;
     animation: gradient 5s ease infinite;
+    transition: all 0.2s ease-in-out;
     img {
       position: relative;
       z-index: 1;
@@ -38,6 +48,28 @@ position: relative;
     p {
       font-size: 2rem;
     }
+    &:hover{
+      animation: boxShadow 5s ease infinite;
+    }
+
+    @media screen and (max-width: 446px) {
+      width: 100%;
+      max-width: none;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      transform: unset;
+      border-radius: unset;
+      img {
+        width: 100px;
+        height: 100px;
+      }
+      &:hover {
+        animation: none;
+        background: transparent;
+      }
+    }
   }
   .carousel {
     position: relative;
@@ -53,7 +85,7 @@ position: relative;
       margin: auto 0;
       z-index: -1;
       @media screen and (max-width: 768px) {
-        height: 90vh;
+        height: 100vh;
       }
 
       @media screen and (max-width: 426px) {
@@ -66,20 +98,6 @@ position: relative;
     margin: 0;
     }
   }
-
-
-
-  h1 {
-    display: inline-block;
-    position: absolute;
-    z-index: 3;
-    font-size: 4rem;
-    padding: 1rem;
-    @media screen and (max-width: 769px) {
-      font-size:${(props) => (props.massage ? '2.4rem' : '2.5rem')};
-      bottom:${(props) => (props.massage ? '0' : '5% !important')} ;
-      top:${(props) => (props.massage ? '15% !important' : '60% !important')};
-    }
 
     @media screen and (max-width: 426px) {
       font-size: 1.5rem;
