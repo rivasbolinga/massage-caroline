d8c1737df49914e95205a88640de8bc47840cd75
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-22 21:33:20 +0530

    Feedback init

--- src/components/features/home-sliders/Feedback.jsx
+++ src/components/features/home-sliders/Feedback.jsx
@@ -1,111 +1,71 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './styles.scss';
 import TitleWrap from '../../styles/HomeTitle';
 
-class Feedback extends Component {
-  constructor() {
-    super();
-    this.state = {
-      slides: [
-        {
-          img: 'https://dummyimage.com/400x300/fff/7CFC00',
-          feedback:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!',
+const Feedback = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false,
         },
-        {
-          img: 'https://dummyimage.com/400x300/0f8/ccccc',
-          feedback:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!',
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          dots: false,
         },
-        {
-          img: 'https://dummyimage.com/400x300/595/dddddd',
-          feedback:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!',
+      },
+      {
+        breakpoint: 426,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
         },
-        {
-          img: 'https://dummyimage.com/400x300/808/fff',
-          feedback:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dignissimos aut, eius nulla rerum perferendis quidem eligendi, tempora, consequuntur ab obcaecati doloribus aliquid eos recusandae ipsum repellat sed vel facilis!',
-        },
-      ],
-    };
-  }
-
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 2,
-      initialSlide: 0,
-      autoplay: false,
-      autoplaySpeed: 2000,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 2,
-            infinite: true,
-            dots: false,
-          },
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2,
-            dots: false,
-          },
-        },
-        {
-          breakpoint: 426,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: false,
-          },
-        },
-      ],
-    };
-
-    const { slides } = this.state;
-    return (
-      <div id="feedback">
-        <TitleWrap className="about-title">
-          <h2>Costumer Feedback</h2>
-        </TitleWrap>
-        <Slider
-          dots={settings.dots}
-          infinite={settings.infinite}
-          speed={settings.speed}
-          slidesToShow={settings.slidesToShow}
-          slidesToScroll={settings.slidesToScroll}
-          initialSlide={settings.initialSlide}
-          autoplay={settings.autoplay}
-          autoplaySpeed={settings.autoplaySpeed}
-          responsive={settings.responsive}
-        >
-          {slides.map(({ img, feedback }, index) => (
-            <div
-              data-aos="flip-right"
-              data-aos-duration="2000"
-              key={img}
-              className="image-wrap"
-            >
-              <img src={img} alt={`slide${index}`} />
-              <div className="quotes">{feedback}</div>
-            </div>
-          ))}
-        </Slider>
-      </div>
-    );
-  }
-}
+      },
+    ],
+  };
+  return (
+    <div>
+      <Slider
+        dots={settings.dots}
+        infinite={settings.infinite}
+        speed={settings.speed}
+        slidesToShow={settings.slidesToShow}
+        slidesToScroll={settings.slidesToScroll}
+        initialSlide={settings.initialSlide}
+        autoplay={settings.autoplay}
+        autoplaySpeed={settings.autoplaySpeed}
+        responsive={settings.responsive}
+      >
+        <div>
+          <TitleWrap>ahgfdsjghfjsdf</TitleWrap>
+          <div className="descriptions">
+            adfgkdsjfdghskjfgahdgkhahsgfjldsfadgfda
+          </div>
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
 export default Feedback;
