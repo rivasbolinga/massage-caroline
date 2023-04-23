1bf6b1601a8dfc9a5e582f8d7ecfad1cf113422e
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-04-23 17:43:33 +0530

    desktop feedback add responsiveness

--- src/components/features/home-sliders/FeedbackCard.jsx
+++ src/components/features/home-sliders/FeedbackCard.jsx
@@ -11,11 +11,20 @@ const FeedbackCard = ({
       <img key={id} src={image} alt={name} />
     </div>
     <div key={index} className="star-name">
-      <span>{name}</span>
+      <i className="fa-solid fa-quote-right quote fa-beat" />
+      <h3>{name}</h3>
       <q className="description">
         {description}
       </q>
-      <span>{star}</span>
+      <span>
+        {() => {
+          for (let i = 0; i < 5; i += 1) {
+            if (i <= star) <i className="fa-solid fa-star" />;
+              <i className="fa-solid fa-star" />;
+          }
+        }}
+      </span>
+      <i className="fa-solid fa-quote-left quote fa-beat" />
     </div>
   </FeedbackCardWrap>
 );
@@ -25,10 +34,12 @@ const FeedbackCardWrap = styled.div`
   margin: auto auto;
   width: 70%;
   flex-wrap: wrap;
-  border: 1px solid black;
+  border: 1px inset #f9a392;
   border-radius: 2rem;
   overflow: hidden;
   gap: 10px;
+  box-shadow: 5px 5px 15px #30221e;
+  translation: all 1s ease-in-out;
   .img-wrap {
     width: 40vh;
     overflow: hidden;
@@ -36,11 +47,17 @@ const FeedbackCardWrap = styled.div`
   img {
     width: 100%;
     height: 50vh;
+    transition: all 1s ease;
   }
   .star-name {
     width: 50%;
     display: flex;
     flex-direction: column;
+    gap: 1rem;
+    padding: 1rem;
+    align-items: center;
+    justify-content: center;
+    position: relative;
   }
 
   &:hover {
@@ -48,6 +65,19 @@ const FeedbackCardWrap = styled.div`
       overflow: hidden;
       transform: scale(1.1, 1.1);
     }
+  }
+  .quote {
+    position: absolute;
+    font-size: 1.8rem;
+  }
+
+  .fa-quote-right {
+    top: 10%;
+    right: 10%;
+  }
+  .fa-quote-left {
+    bottom: 10%;
+    left: 10%;
   }
 `;
 FeedbackCard.propTypes = {
