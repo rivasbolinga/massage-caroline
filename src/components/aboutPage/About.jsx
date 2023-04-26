642b001930d12b0dbeed76905fe89f57ffadf6e2
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-04-26 20:24:38 +0200

    Tablet syle

--- src/components/aboutPage/About.jsx
+++ src/components/aboutPage/About.jsx
@@ -9,26 +9,29 @@ const About = () => {
   const { t } = useTranslation();
   return (
     <>
-      <main
-        data-aos="fade-up"
-        data-aos-duration="1000"
-      >
+      <main data-aos="fade-up" data-aos-duration="1000">
         <div className="about-me-container">
           <h1 className="about-me-title">{t('about.about-me')}</h1>
           <h3 className="about-me-name">{t('about.name')}</h3>
-          <img className="image-about" alt="me" src={me} />
-          <div className="about-me-text">
-            <p>{t('about.my-work')}</p>
-            <br />
-            <p>{t('about.techniques')}</p>
-            <br />
-            <p>{t('about.passionate')}</p>
-          </div>
-          <div className="button-container">
-            <Link to="/massages#top" type="button" className="massages-button">
-              {t('about.go-massages')}
-              <BsArrowRight />
-            </Link>
+          <div className="about">
+            <img className="image-about" alt="me" src={me} />
+            <div className="about-me-text">
+              <p>{t('about.my-work')}</p>
+              <br />
+              <p>{t('about.techniques')}</p>
+              <br />
+              <p>{t('about.passionate')}</p>
+            </div>
+            <div className="button-container">
+              <Link
+                to="/massages#top"
+                type="button"
+                className="massages-button"
+              >
+                {t('about.go-massages')}
+                <BsArrowRight />
+              </Link>
+            </div>
           </div>
         </div>
       </main>
